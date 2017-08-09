6d88bf298047f1a23bfb5cc79d336b3a4cd8ef7f
nikolaj schlüter.nielsen <nikolaj schlter.nielsen>
2017-08-09 02:02:31 +0200

    add menu

--- src/script.js
+++ src/script.js
@@ -1,7 +1,42 @@
-$(".frontpage-text").ready(function(){
+function sizeCorrectly() {
 	var contentHeight = $(".floater").height();
 	$(".frontpage-text").css("top", window.innerHeight/2 - contentHeight/2 + "px")
+}
+
+
+$(".frontpage-text").ready(function(){
+sizeCorrectly()
 })
+
+$( window ).resize(function() {
+sizeCorrectly()
+});
+
 
 
 $(".frontpage-text").css("top", window.innerHeight + "px")
+
+
+$(document).ready(function(){
+	$(".floater").hover(function(){
+		//menu:
+		$(".menu").stop()
+		$(".menu").animate({opacity: 1, top:"0px" }, 1000, "easeOutExpo");
+
+		//logo:
+		$("#slyt").stop()
+		console.log('test')
+		$("#slyt").animate({width: "120px", top:"-50px" }, 1000, "easeOutExpo");
+	}, function(){
+		//menu:
+		$(".menu").stop()
+		$(".menu").animate({opacity: 0, top:"-30px" }, 1000, "easeInOutExpo");
+
+		//logo:
+		$("#slyt").stop()
+		$("#slyt").animate({width: "230px", top:"0px" }, 1000, "easeInOutExpo");
+
+	})
+	
+})
+
