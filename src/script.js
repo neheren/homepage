4b7f22fe1997fb5d60d3077651a73534538e9bd8
nikolaj schlüter.nielsen <nikolaj schlter.nielsen>
2017-08-09 04:10:54 +0200

    fix menu stuff and add dot

--- src/script.js
+++ src/script.js
@@ -3,22 +3,55 @@ function sizeCorrectly() {
 	$(".frontpage-text").css("top", window.innerHeight/2 - contentHeight/2 + "px")
 }
 
+function videoHeight() {
+	$(".video").css("height", window.innerHeight);
+}
 
 $(".frontpage-text").ready(function(){
 sizeCorrectly()
+videoHeight()
 })
 
 $( window ).resize(function() {
 sizeCorrectly()
+videoHeight();
 });
 
 
 
 $(".frontpage-text").css("top", window.innerHeight + "px")
 
+puls = true;
+
+function pulse(obj){
+	if(puls){
+		$(obj).stop()
+		$(obj).animate({opacity:0}, 1500, "easeInOutExpo", function(){
+			$(obj).animate({opacity:1}, 1500, "easeInOutExpo", function(){
+				pulse(obj)
+			});
+		});
+	}
+}
+
+
+function scrollLink(linkObj, scrollToObj){
+	$(linkObj).click(function() {
+		$('html, body').animate({
+			scrollTop: $(scrollToObj).offset().top
+		}, 1000, "easeInOutExpo");
+	});
+}
 
 $(document).ready(function(){
 	$(".floater").hover(function(){
+
+		$("#ballimg").stop()
+		$("#ballimg").animate({opacity:1}, 500)
+		puls = false;
+		
+		$("#ball").stop()
+		$("#ball").animate({opacity:1, left:"70", top:"-60px", height:"8px"}, 1200, "easeOutExpo")
 		//menu:
 		$(".menu").stop()
 		$(".menu").animate({opacity: 1, top:"0px" }, 1000, "easeOutExpo");
@@ -36,7 +69,25 @@ $(document).ready(function(){
 		$("#slyt").stop()
 		$("#slyt").animate({width: "230px", top:"0px" }, 1000, "easeInOutExpo");
 
+		$("#ball").stop()
+		$("#ball").animate({left:"130px", top:"-65px", height:"12px"}, 1000, "easeInOutExpo")
+
+		puls = true;
+		pulse("#ballimg");
 	})
-	
+
+	$(".smalltext").hover(function(){
+		var item = $(this).position().left + ($(this).width()/2) + 8 ;
+		$("#ball").stop()
+		$("#ball").animate({opacity:1, left: item-(window.innerWidth/2) + "px", top:"0px"}, 500, "easeOutExpo")
+	})
+
+	scrollLink("#fullstacklink", "#fullstackcontent")
 })
+
+
+
+
+
+
 
