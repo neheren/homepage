6139f6888ca655f5c2ebcedca930072ee5313068
nikolaj schlüter.nielsen <nikolaj schlter.nielsen>
2017-08-13 04:23:31 +0200

    add portfolio menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-require('handlebars');
+const express = require('express');
+const Handlebars = require('handlebars');
+
+const app = express();
+
+app.set('view engine', 'handlebars');
+
+
+app.get('/', function (req, res) {
+    const template = Handlebars.compile(import('../views/test.hbs'))
+    res.send(template({}))
+})
+
+app.listen(3000);
