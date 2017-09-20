983e5c3aea23522d71ed4924135fda1e3a7618cc
nikolaj schlüter.nielsen <nikolaj.sn@hotmail.com>
2017-09-20 23:48:09 +0200

    add media queries and use css instead of jQ

--- src/frontpage.js
+++ src/frontpage.js
@@ -54,7 +54,7 @@ function scrollLink(linkObj, scrollToObj){
 
 function hideMenu(){
 	$("#ball").stop().animate({opacity:1, left:"70", top:"-65px", height:"8px"}, 300, "easeOutExpo")
-	$(".menu").stop().animate({opacity: 0, top:"-30px" }, 1000, "easeInOutExpo");
+	//$(".menu").stop().animate({opacity: 0, top:"-30px" }, 1000, "easeInOutExpo");
 }
 
 $(document).ready(function(){
@@ -68,11 +68,11 @@ $(document).ready(function(){
 		puls = false;
 		$("#ball").stop().animate({opacity:1, left:"70px", top:"-70px", height:"8px"}, 1200, "easeOutExpo")
 		//menu:
-		$(".menu").stop().animate({opacity: 1, top:"0px" }, 1000, "easeOutExpo", function(){
+		//$(".menu").stop().animate({opacity: 1, top:"0px" }, 1000, "easeOutExpo", function(){
 			menuDown = true;
-		});
+	//	});
 		//logo:
-		$("#slyt").stop().animate({width: "120px", top:"-50px" }, 1000, "easeOutExpo");
+		//$("#slyt").stop().animate({width: "120px", top:"-50px" }, 1000, "easeOutExpo");
 
 	}, function(){
 		menuDown = false;
@@ -81,14 +81,14 @@ $(document).ready(function(){
 			//ball:
 			$("#ball").stop().animate({left:"130px", top:"-70px", height:"12px"}, 1000, "easeInOutExpo")
 			puls = false;
-			$("#slyt").stop().animate({width: "230px", top:"0px" }, 1000, "easeInOutExpo");
+			//$("#slyt").stop().animate({width: "230px", top:"0px" }, 1000, "easeInOutExpo");
 		}else{
 			//ball:		
 			$("#ball").stop().animate({opacity:1, left:"70", top:"-65px", height:"8px"}, 1200, "easeOutExpo")
 			puls = true;
 			pulse("#ballimg");
 		}
-		$(".menu").stop().animate({opacity: 0, top:"-30px" }, 1000, "easeInOutExpo");
+		//$(".menu").stop().animate({opacity: 0, top:"-30px" }, 1000, "easeInOutExpo");
 	})
 
 	$(".menu-item").hover(function(){
@@ -104,13 +104,13 @@ $(document).ready(function(){
 		if(window.scrollY > menuHeight){
 			$(".frontpage-text").css({top: window.scrollY})
 			$("#ball").stop().animate({opacity:1, left:"70", top:"-70px", height:"8px"}, 1200, "easeOutExpo")
-			$("#slyt").stop().animate({width: "120px", top:"-50px" }, 1000, "easeOutExpo");
+			$(".slyt").addClass('toTop')
 
 			/*ADD CLASS TO WITH ANIMATION IMITING */
 
 		}else{
 			$(".frontpage-text").css({top: menuHeight+ "px"})
-			$("#slyt").stop().animate({width: "230px", top:"0px" }, 1000, "easeInOutExpo");
+			$(".slyt").removeClass('toTop')
 			$("#ball").stop().animate({left:"130px", top:"-70px", height:"12px"}, 1000, "easeInOutExpo")
 		}
 	})
