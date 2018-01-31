6db1314b00b33f1cbe0909399afe981fd35c81c9
nikolaj schlüter.nielsen <nikolaj.sn@hotmail.com>
2018-01-31 03:05:53 +0100

    add timeline text

--- src/timeline.js
+++ src/timeline.js
@@ -7,12 +7,22 @@ $(document).ready(function(){
     $svg = $('#timeline').drawsvg({easing:"easeInOutCubic"});
     // $offset svg. 
     max = $doc.height() - $win.height();
-    
+    $timelines = $(".timeline-text-date");
+
+
     $win.on('scroll', function() {
-        p = Math.max(0, $win.height() + $win.scrollTop() - $svg.position().top)/ ($win.height() + $svg.height());
-        console.log(p)
-        //p = $win.scrollTop() / max;
+        p = Math.max(0, $win.height() + $win.scrollTop() - $svg.position().top)/ ($win.height() + $svg.height() - 200);
         $svg.drawsvg('progress', p);
+        
+        var incr = Math.floor(p * $timelines.length*1.8);
+        
+        $timelines.css({opacity: 0});
+        
+        for(i = 0; i < incr; i++){
+            $($timelines[i]).css({opacity: 1});
+
+        }
+
     });
 
 })
