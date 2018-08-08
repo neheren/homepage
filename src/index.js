a27719d8eb3f6e7258916f007b5a66d2b7f89722
Nikolaj <nikolaj.sn@hotmail.com>
2018-08-08 16:05:01 +0000

    NON WORKING STATE, content is messed up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const pug = require('pug');
-
+const port = 3000
 const app = express();
 
 function article(id, title, video, image, text, desc, date){
@@ -9,7 +9,12 @@ function article(id, title, video, image, text, desc, date){
 
 const articles = [
     article(0, 'Mix And Match', 'https://www.youtube.com/embed/O3w1uUlgplE', 'mixand', 'En Roskilde installation med formål at bringe folk sammen over "up and comming" musik. Udover at være med at til projektet har jeg både animeret og produceret videoen. ', 'Lavet på Medialogy 3. semester', '13/8-2016'), 
-    article(1, 'LUX - A videogame about darkness', 'https://www.youtube.com/embed/za5KZ1Ldn2g', 'lux', 'Et computerspil der omhandler mørke udforskning', '', '13/8-2017'), 
+    article(1, 'LUX - A videogame about darkness', 'https://www.youtube.com/embed/za5KZ1Ldn2g', 'lux', 'Opgaven havde som formål at udvikle et spil, der kunne variere i sværhedsgrader afhængig af spillerens eget niveau, kompetencer og formåen. 
+Spillet skulle derfor kunne kalibrere sværhedsgraden for en ny spiller og ydermere proportionelt stige i sværhedsgrad i takt med, at spilleren selv blev bedre.
+Vi valgte derfor at bygge spillet op omkring, at en first-person skulle være placeret i et næsten-fuldstændig mørkt univers. Spilleren ville få en kugle med lys, hvilket muliggjorde, at spilleren kunne se spiluniverset, hvis den blev kastet. Omfanget af hvor oplyst, lyskuglen formåede at gøre spiluniverset, afhang derfor af spillernes evne til at navigere rundt i de forskellige mængder af lys.', 'Jeg arbejdede her især med:
+DDA - Dynamic Difficulty Adjustment - at kunne udvikle et spil med en dynamisk sværhedsgrad, der konsistent skal kunne udfordre spilleren. Programmering - fokus på (ud over basal programmering og kode) at sætte programmering i et selvlærende-perspektiv.
+Algoritmer - udvikle algoritmen bag sværhedgradsoptimering.
+Eksperimentdesign - samle kvantitativ data om spillerens formåen samt finde ud af, hvordan spillerne lader lyset påvirke spillerens evne til at udføre spillet.', '13/8-2017'), 
     article(2, 'Embodied Metaphors & Audio Effects', 'https://www.youtube.com/embed/TuVOLyqg7v4', 'embodied', 'I denne smag bruger vi sag.', 'DET RIGTIGT -Sivas 2017', '13/8-2017'), 
     article(3, 'DSI - A Small timelapse music video', 'https://www.youtube.com/embed/Q2h3qKUEP70', 'dsi', 'Tjek det ud', 'DET RIGTIGT -Sivas 2017', '13/8-2017'), 
     article(4, 'Simply Sketch', 'https://www.youtube.com/embed/IxLoqeQ_tRg', 'crowd', 'I denne smag bruger vi sag.', 'DET RIGTIGT -Sivas 2017', '13/8-2017'), 
@@ -43,4 +48,5 @@ app.get('/:content/:id', function (req, res) {
 app.use(express.static('public'))
 app.use(express.static('files'))
 
-app.listen(3000);
+app.listen(port);
+console.log('Server listening on port' + port)
